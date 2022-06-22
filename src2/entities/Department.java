00fdc9870c914659d1c7e037bbda8f2dc94e0316
MarcioRCH <marciorchsilva@gmail.com>
2022-06-22 17:33:42 -0300

    Update entities

--- src2/entities/Department.java
+++ src2/entities/Department.java
@@ -1,16 +1,25 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Department {
 	
 	private String name;
 	private Integer payDay;
+	private Employee employee;
+	private Address address;
+	
+	private List<Employee> emps = new ArrayList<>();
 	
 	public Department() {
 		
 	}
-	public Department(String name, Integer payDay) {
+	public Department(String name, Integer payDay, Employee employee, Address address) {
 		this.name = name;
 		this.payDay = payDay;
+		this.employee = employee;
+		this.address = address;
 	}
 	public String getName() {
 		return name;
@@ -23,5 +32,26 @@ public class Department {
 	}
 	public void setPayDay(Integer payDay) {
 		this.payDay = payDay;
+	}
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public List<Employee> getEmp(){
+		return emps;
+	}
+	public void addEmp(Employee emp) {
+		emps.add(emp);
+	}
+	public void removeEmp(Employee emp) {
+		emps.remove(emp);
 	}
 }
